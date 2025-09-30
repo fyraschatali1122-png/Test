337f19740d8110a5a19669ba0f28511453933834
fyraschatali1122-png <fyraschatali1122@gmail.com>
2025-09-30 02:18:13 +0200

    config.js

--- Config.js
+++ Config.js
@@ -1,4 +1,4 @@
 window.CONFIG = {
   CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTOuHwb0HGELAZhFI3d4vrmWxevqbwwzPgTIOD8TIGII_x7oSsJleuGLdX4cYb5V373IX_Rn1RPrh_Q/pub?gid=569786249&single=true&output=csv",
-  WEBAPP_URL: "https://script.google.com/macros/s/AKfycbzjLpFTbPsrye8ZbxS0Wiqgyusw9FWYcVk3hM6buVBnuTMsBR36pTfyXurRFUK1I0iK2w/exec" // celui qui marche déjà pour Requests
+  WEBAPP_URL: "https://script.google.com/macros/s/AKfycbyeVOAGPD8aeXlEq3zka2dtCHw--5kFKKDN00Esq6-VpdE1apPagEqnyIUs2C2umN8sHQ/exec"
 };
